3fb17a01148802936b5512f0cfc4cb01bd039b34
Your Name <tranphupham2909@gmail.com>
2025-04-09 03:47:50 +0700

    hello

--- js/js.js
+++ js/js.js
@@ -1,13 +1,4 @@
 function toggleMenu() {
     const menu = document.getElementById("mobile-menu");
-
-    // Kiểm tra nếu đã có lớp 'active' nghĩa là đã nhấn rồi
-    if (menu.classList.contains("active")) {
-        console.log("Đã nhấn nút ☰ - menu đang mở");
-    } else {
-        console.log("Chưa nhấn hoặc menu đang đóng");
-    }
-
-    // Toggle menu
     menu.classList.toggle("active");
 }
