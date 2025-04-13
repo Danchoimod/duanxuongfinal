c43794541c04389796c0aecdb6b3b801faa0b5a3
Your Name <tranphupham2909@gmail.com>
2025-04-13 23:19:16 +0700

    hello

--- js/js.js
+++ js/js.js
@@ -2,3 +2,9 @@ function toggleMenu() {
     const menu = document.getElementById("mobile-menu");
     menu.classList.toggle("active");
 }
+    
+document.querySelector('.pic1').addEventListener('mouseenter', function () {
+    document.querySelector('#diem-den').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
